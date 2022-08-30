caa3985ae02faefd0cd1e3e78ba32c42cce47e30
hoangkimcalan <34814759+hoangkimcalan@users.noreply.github.com>
2022-08-30 16:35:24 +0700

    Update [id].jsx

--- pages/posts/detail/[id].jsx
+++ pages/posts/detail/[id].jsx
@@ -47,7 +47,7 @@ function Detail() {
                 className='w-full p-8 flex flex-col justify-between h-auto overflow-auto lg:h-auto'
             >
                 <div className="flex gap-1">
-                    <h1 class='text-left text-3xl font-bold leading-normal'>
+                    <h1 className='text-left text-3xl font-bold leading-normal'>
                         {currentPost.title}
                     </h1>
                     <tag className="text-emerald-300 bg-green-600 h-8 rounded-md">{currentPost.category}</tag>
@@ -60,7 +60,7 @@ function Detail() {
                 <div className='flex gap-4'>
                     <div className="text-red-600 text-xl flex items-center gap-2">
                         <div className="cursor-pointer" onClick={() => handleIncrementLike()}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
                                 <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
                             </svg>
                         </div>
@@ -68,7 +68,7 @@ function Detail() {
                     </div>
                     <div className="text-gray-800 flex text-xl items-center gap-2">
                         <div className="cursor-pointer">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye" viewBox="0 0 16 16">
                                 <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
                                 <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
                             </svg>
@@ -78,7 +78,7 @@ function Detail() {
 
                     <div className="text-gray-800 flex text-xl items-center gap-2">
                         <div className="cursor-pointer">
-                            {/* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
+                            {/* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-share" viewBox="0 0 16 16">
                                 <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z" />
                             </svg> */}
 
